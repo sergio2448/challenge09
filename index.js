0125a69a73eddac719db1edb64287081d8e7afd6
sergio2448 <84557725+sergio2448@users.noreply.github.com>
2022-09-21 23:18:26 -0500

    add: views

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const apiRoutes = require("./routers/app.routers");
 const loggerMiddleware = require("./middlewares/logger");
 
+const Container = require("./models/Container.js");
+const productApi = new Container("./data/data1.json");
+
 const PORT = process.env.PORT || 8080;
 const app = express();
 
@@ -9,11 +12,27 @@ const app = express();
 app.use(loggerMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+//app.use(express.static("public"));
+app.set("views", "./views");
+
+app.set("view engine", "pug");
 
 // Routes
 app.use("/api", apiRoutes);
 
+app.get("/products", async (req, res) => {
+  const products = await productApi.getAll();
+  res.render("products.pug", { products: JSON.parse(products) });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/save", async (req, res) => {
+  await productApi.save(req.body);
+  res.redirect("/");
+});
 // Server
 const serverOn = app.listen(PORT, () => {
   console.log(`SERVER ON, listening on PORT: ${PORT}`);
