8d32ac9f9999707e60e4893330b4d3bedc934e62
sergio2448 <84557725+sergio2448@users.noreply.github.com>
2022-09-25 18:00:16 -0500

    fix: save method and route views

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const loggerMiddleware = require("./middlewares/logger");
 const Container = require("./models/Container.js");
 const productApi = new Container("./data/data1.json");
 const path = require("path");
-
-const { engine } = require("express-handlebars");
+const hbs = require("hbs");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -16,27 +15,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(express.static("public"));
 
-app.engine(
-  "hbs",
-  engine({
-    extname: "hbs",
-    defaultLayout: "main.hbs",
-    layoutsDir: path.resolve(__dirname, "./views/layouts"),
-    partialsDir: path.resolve(__dirname, "./views/partials"),
-  })
-);
-app.set("views", "./views");
-
-app.set("view engine", "handlebars");
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 // Routes
 app.use("/api", apiRoutes);
 
 app.get("/products", async (req, res) => {
-  let products = await productApi.getAll();
-  let hasProducts = products.length > 3 ? true : false;
-  console.log(hasProducts);
-  res.render("index.hbs", { hasProducts, products: JSON.parse(products) });
+  const getProducts = await productApi.getAll();
+  const products =
+    getProducts.length > -1 && getProducts.length < 4
+      ? false
+      : JSON.parse(getProducts);
+  res.render("products.hbs", {
+    products,
+  });
 });
 
 app.get("/", (req, res) => {
